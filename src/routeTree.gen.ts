83353ccb46de949ca23e37e3685edb746df833c6
muhammedshibili-kp <muhammedshibili7425@gmail.com>
2026-04-28 17:25:20 +0530

    fix: convert to SPA

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -78,4 +78,9 @@ export const routeTree = rootRouteImport
 
 import type { getRouter } from './router.tsx'
 import type { createStart } from '@tanstack/react-start'
-
+declare module '@tanstack/react-start' {
+  interface Register {
+    ssr: true
+    router: Awaited<ReturnType<typeof getRouter>>
+  }
+}
